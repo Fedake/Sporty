8f94366088de90fe000f9ea2209c285c90e3a7aa
Fedake <braveside@gmail.com>
2012-11-21 21:02:55 +0100

    Dodany damping, zwiększone friction(ułatwienie łyżki)

--- Ball.cpp
+++ Ball.cpp
@@ -6,13 +6,15 @@ Ball::Ball(b2Vec2 pos, b2World* world, sf::RenderWindow* win) : m_world(world), 
 	// Body
 	m_bodyDef.type = b2_dynamicBody;
 	m_bodyDef.position.Set(6.0f, 0.0f);
+	m_bodyDef.linearDamping = 0.3;
+	m_bodyDef.bullet = true;
 	m_body = m_world->CreateBody(&m_bodyDef);
-
-	m_dynamicBox.m_radius = 0.25f;
 	
+	m_dynamicBox.m_radius = 0.25f;
+
 	m_fixtureDef.shape = &m_dynamicBox;
 	m_fixtureDef.density = 0.2f;
-	m_fixtureDef.friction = 0.3f;
+	m_fixtureDef.friction = 0.5f;
 	m_fixtureDef.restitution = 0.5f;
 	m_body->CreateFixture(&m_fixtureDef);
 
